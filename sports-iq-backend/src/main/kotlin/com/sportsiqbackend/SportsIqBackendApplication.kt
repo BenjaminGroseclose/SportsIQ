18cc2b89d55a6e7c8825524e1d2b43b2f1d71344
Ben Groseclose <ben.groseclose01@gmail.com>
2025-01-17 10:48:55 -0500

    Init Commit

---
+++ sports-iq-backend/src/main/kotlin/com/sportsiqbackend/SportsIqBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.sportsiqbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SportsIqBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SportsIqBackendApplication>(*args)
+}
---
+++ sports-iq-backend/src/test/kotlin/com/sportsiqbackend/SportsIqBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sportsiqbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SportsIqBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
