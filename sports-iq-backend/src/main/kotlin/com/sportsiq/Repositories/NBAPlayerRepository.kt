a6db15ff695dfee5228facd05aa42208f9c51fa4
Ben Groseclose <ben.groseclose01@gmail.com>
2025-01-23 22:34:13 -0500

    Delete Wrong Files

--- sports-iq-backend/src/main/kotlin/com/sportsiq/Repositories/NBAPlayerRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.sportsiq.Repositories
-
-import com.sportsiq.Models.NBAPlayer
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface NBAPlayerRepository: MongoRepository<NBAPlayer, String> { }
