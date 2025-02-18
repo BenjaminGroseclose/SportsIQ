bac8c672b4563636ea2b9b6c49069dfe18f0e6b3
Ben Groseclose <ben.groseclose01@gmail.com>
2025-02-18 14:51:42 -0500

    Frontend Dockerfile

--- sports-iq/src/app/app.routes.ts
+++ sports-iq/src/app/app.routes.ts
@@ -1,23 +1,24 @@
-import { Routes } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.component';
-import { StatsComponent } from './pages/stats/stats.component';
-import { RankingComponent } from './pages/ranking/ranking.component';
+import { Routes } from "@angular/router";
+import { LandingComponent } from "./pages/landing/landing.component";
+import { StatsComponent } from "./pages/stats/stats.component";
+import { RankingComponent } from "./pages/ranking/ranking.component";
+import { RenderMode } from "@angular/ssr";
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    title: 'Sports IQ - Home',
-    component: LandingComponent,
-  },
-  {
-    path: ':sport/stats',
-    title: 'Sports IQ - Stats',
-    component: StatsComponent,
-  },
-  {
-    path: ':sport/ranking',
-    title: 'Sports IQ - Stats',
-    component: RankingComponent,
-  },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+	{
+		path: "home",
+		title: "Sports IQ - Home",
+		component: LandingComponent
+	},
+	{
+		path: ":sport/stats",
+		title: "Sports IQ - Stats",
+		component: StatsComponent
+	},
+	{
+		path: ":sport/ranking",
+		title: "Sports IQ - Ranking",
+		component: RankingComponent
+	},
+	{ path: "", redirectTo: "/home", pathMatch: "full" }
 ];
